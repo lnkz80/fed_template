c1e0aaadefc58b24b357371df4b65fdf0bc4407d
lnkz80 <lnkz80@gmail.com>
2017-03-23 16:04:21 +0200

    add obj

--- app/js/common.js
+++ app/js/common.js
@@ -1,10 +1,44 @@
 $(function() {
 
-	$(".toggle-mnu").click(function() {
-  $(this).toggleClass("on");
-  $(".main-mnu").slideToggle();
-  return false;
+$(".toggle-mnu").click(function() {
+	$(this).toggleClass("on");
+	$(".main-mnu").slideToggle();
+	return false;
+});
+
+//ANIMATE NUMBERS IN SECTION
+$(".advantages").waypoint(function(){
+	// $( ".item-adv b" ).each(function() {
+	// 	$(this).animate({opacity: .5}, 500 );
+	// });
+	$("div.item-adv b").each(function() {
+	var tcount = $(this).data("count");
+	$(this).animateNumber({ number: tcount,
+		easing: 'easeInQuad'},
+		1500);
 	});
+	}, {
+	offset: '70%'	
+});
+
+//PARALLAX EFFECT IN SECTION
+$(".advantages").stellar();
+
+//BUILD OBJECTS ITEM HOVER EFFECT
+$("div.item-obj").hover(function() {
+	var addp = $(this).find(".descr-obj-wrapper");
+	var objd = $(this).find(".ico-obj-descr");
+	objd.animate({width: 'toggle'});
+	addp.slideToggle(400);
+	
+	return false;
+});
+
+// !!!!!!!!!!!!!!!!FOR TESTS!!!!!!!!!!!!!!!!
+// $(".item-adv span").click(function(){
+// 	alert("dfnhjsgf");
+// });
+
 
 	//SVG Fallback
 	if(!Modernizr.svg) {
