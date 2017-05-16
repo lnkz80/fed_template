f318fb8ff464f2ee0cb6a7010d20e0cdb21b3e32
lnkz80 <lnkz80@gmail.com>
2017-05-16 14:27:35 +0300

    making_slider
    видно загружаемый контент, который должен быть скрыт.

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,68 @@
+// SLIDER ==================================================================================
+// var hwSlideSpeed = 700;
+// var hwTimeOut = 3000;
+// var hwNeedLinks = true;
+ 
+// $(document).ready(function(e) {
+//     $('.posttext').css(
+//         {"position" : "absolute",
+//          "top":'0', "left": '0'}).hide().eq(0).show();
+//     var slideNum = 0;
+//     var slideTime;
+//     slideCount = $("#slider .slide").size();
+//     var animSlide = function(arrow){
+//         clearTimeout(slideTime);
+//         $('.slide').eq(slideNum).fadeOut(hwSlideSpeed);
+//         if(arrow == "next"){
+//             if(slideNum == (slideCount-1)){slideNum=0;}
+//             else{slideNum++}
+//             }
+//         else if(arrow == "prew")
+//         {
+//             if(slideNum == 0){slideNum=slideCount-1;}
+//             else{slideNum-=1}
+//         }
+//         else{
+//             slideNum = arrow;
+//             }
+//         $('.slide').eq(slideNum).fadeIn(hwSlideSpeed, rotator);
+//         $(".control-slide.active").removeClass("active");
+//         $('.control-slide').eq(slideNum).addClass('active');
+//         }
+// if(hwNeedLinks){
+// var $linkArrow = $('<a id="prewbutton" href="#"></a><a id="nextbutton" href="#"></a>')
+//     .prependTo('#slider');      
+//     $('#nextbutton').click(function(){
+//         animSlide("next");
+ 
+//         })
+//     $('#prewbutton').click(function(){
+//         animSlide("prew");
+//         })
+// }
+//     var $adderSpan = '';
+//     $('.slide').each(function(index) {
+//             $adderSpan += '<span class = "control-slide">' + index + '</span>';
+//         });
+//     $('<div class ="sli-links">' + $adderSpan +'</div>').appendTo('#slider-wrap');
+//     $(".control-slide:first").addClass("active");
+     
+//     $('.control-slide').click(function(){
+//     var goToNum = parseFloat($(this).text());
+//     animSlide(goToNum);
+//     });
+//     var pause = false;
+//     var rotator = function(){
+//     if(!pause){slideTime = setTimeout(function(){animSlide('next')}, hwTimeOut);}
+//             }
+//     $('#slider-wrap').hover(    
+//         function(){clearTimeout(slideTime); pause = true;},
+//         function(){pause = false; rotator();
+//         });
+//     rotator();
+// });
+// ================================================================================== EoF SLIDER 
+
 $(function() {
 
 $(".toggle-mnu").click(function() {
@@ -6,7 +71,46 @@ $(".toggle-mnu").click(function() {
 	return false;
 });
 
-//ANIMATE NUMBERS IN SECTION
+//MINE SLIDER ====================================================================
+
+	$(".posttext").hide().eq(0).show();
+	$(".postheader span").hide().eq(0).show();
+	var slNum = 0;
+	var totalSl = 2; //Sliders - 1
+	function slideItems(z){
+		if(slNum<0){
+			slNum=totalSl;
+		}
+		if(slNum>totalSl){
+			slNum = 0;
+		}
+		if(z=="+"){
+			prevNum = slNum + 1;
+			if(prevNum>totalSl){
+				prevNum=0;
+			}
+		}
+		else {
+			prevNum = slNum - 1;	
+		}
+		$(".posttext").hide().eq(slNum).fadeIn(700);
+		$(".postheader span").hide().eq(slNum).fadeIn(700);
+		$(".dots i").eq(prevNum).removeClass("active");
+		$(".dots i").eq(slNum).addClass("active");
+		// console.log(slNum + z + 1);
+	}
+	$(".larr i").click(function(){
+		slNum -= 1;
+		slideItems("+");
+	});
+	$(".rarr i").click(function(){
+		slNum += 1;
+		slideItems("-");
+	});
+
+// ================================================================================== EoF
+
+//ANIMATE NUMBERS IN SECTION ====================================================================
 $(".advantages").waypoint(function(){
 	// $( ".item-adv b" ).each(function() {
 	// 	$(this).animate({opacity: .5}, 500 );
@@ -20,9 +124,12 @@ $(".advantages").waypoint(function(){
 	}, {
 	offset: '70%'	
 });
+// ================================================================================== EoF
 
-//PARALLAX EFFECT IN SECTION
+
+//PARALLAX EFFECT IN SECTION ==================================================================
 $(".advantages").stellar();
+$(".news").stellar();
 
 //BUILD OBJECTS ITEM HOVER EFFECT
 $("div.item-obj").hover(function() {
@@ -35,7 +142,7 @@ $("div.item-obj").hover(function() {
 });
 
 // !!!!!!!!!!!!!!!!FOR TESTS!!!!!!!!!!!!!!!!
-// $(".item-adv span").click(function(){
+// $(".logo").click(function(){
 // 	alert("dfnhjsgf");
 // });
 
