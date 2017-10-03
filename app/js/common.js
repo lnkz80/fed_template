1d35a10ce774cccac40a588d2b93a58ce20b9724
lnkz80 <lnkz80@gmail.com>
2017-10-03 16:43:48 +0300

    backup 03-10-2017
    home+about

--- app/js/common.js
+++ app/js/common.js
@@ -162,6 +162,70 @@ $("div.co-worker").hover(function() {
 	return false;
 });
 
+//Magnific Popup OnePicture
+$(".mfp-onepic").magnificPopup({
+	mainClass: 'mfp-zoom-in',	
+	type: 'image',
+	tLoading: '',
+	gallery:{
+		enabled:false,
+	},
+	removalDelay: 300,
+	callbacks: {
+		beforeChange: function() {
+			this.items[0].src = this.items[0].src + '?=' + Math.random(); 
+		},
+		open: function() {
+			$.magnificPopup.instance.next = function() {
+				var self = this;
+				self.wrap.removeClass('mfp-image-loaded');
+				setTimeout(function() { $.magnificPopup.proto.next.call(self); }, 120);
+			}
+			$.magnificPopup.instance.prev = function() {
+				var self = this;
+				self.wrap.removeClass('mfp-image-loaded');
+				setTimeout(function() { $.magnificPopup.proto.prev.call(self); }, 120);
+			}
+		},
+		imageLoadComplete: function() { 
+			var self = this;
+			setTimeout(function() { self.wrap.addClass('mfp-image-loaded'); }, 16);
+		}
+	}
+});
+
+//Maginfic Popup Gallery
+$(".mfp-gallery").magnificPopup({
+	mainClass: 'mfp-zoom-in',	
+	type: 'image',
+	tLoading: '',
+	gallery:{
+		enabled:true,
+	},
+	removalDelay: 300,
+	callbacks: {
+		beforeChange: function() {
+			this.items[0].src = this.items[0].src + '?=' + Math.random(); 
+		},
+		open: function() {
+			$.magnificPopup.instance.next = function() {
+				var self = this;
+				self.wrap.removeClass('mfp-image-loaded');
+				setTimeout(function() { $.magnificPopup.proto.next.call(self); }, 120);
+			}
+			$.magnificPopup.instance.prev = function() {
+				var self = this;
+				self.wrap.removeClass('mfp-image-loaded');
+				setTimeout(function() { $.magnificPopup.proto.prev.call(self); }, 120);
+			}
+		},
+		imageLoadComplete: function() { 
+			var self = this;
+			setTimeout(function() { self.wrap.addClass('mfp-image-loaded'); }, 16);
+		}
+	}
+});
+
 // !!!!!!!!!!!!!!!!FOR TESTS!!!!!!!!!!!!!!!!
 // $(".logo").click(function(){
 // 	alert("dfnhjsgf");
